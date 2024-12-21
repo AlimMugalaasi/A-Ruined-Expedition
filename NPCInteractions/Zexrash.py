0dac21e5767abdb43ee656235dfe7a234912285e
ItsTheEasyPilot <theeasypilot@yahoo.com>
2024-12-21 14:29:58 +0000

    Fixed typo in comment

--- NPCInteractions/Zexrash.py
+++ NPCInteractions/Zexrash.py
@@ -1,6 +1,6 @@
 import sys
 import os
-# Getting the parent directory of the current folder (so i can import Extra Functions)
+# Getting the parent directory of the current folder (so i can import ExtraFunctions)
 parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 sys.path.append(parent_dir)
 from ExtraFunctions import type, newline, clr, sleep
