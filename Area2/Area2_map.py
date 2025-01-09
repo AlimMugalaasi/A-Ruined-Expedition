02eb8140e0fb4ecf6a1c8247c4a33b5386857d02
ItsTheEasyPilot <theeasypilot@yahoo.com>
2025-01-09 11:08:55 +0000

    Z2 WORK and small fixes

--- Area2/Area2_map.py
+++ Area2/Area2_map.py
@@ -37,40 +37,9 @@ A2Z1 = '''
                                                                 % #@&&         
                                                                 # %&# %         
 '''
-CRD_A2Z1 = {
-    (0,0) : GameAssets.A2Z1_Start,
-    (1,0) : GameAssets.A2Z1_a,
-    (1,-1) : GameAssets.A2Z1_b,
-    (2,-1) : GameAssets.A2Z1_c,
-    (2,-2) : GameAssets.A2Z1_Chest,
-    (3,-1) : GameAssets.A2Z1_d,
-    (3,-2) : GameAssets.A2Z1_e,
-    (4,-2) : GameAssets.A2Z1_f,
-    (5,-2) : GameAssets.A2Z1_g,
-    (5,-3) : GameAssets.A2Z1_h,
-    (6,-3) : GameAssets.A2Z1_i
-}
-
-CRD_A2Z1_CRT = {
-    (0,0) : GameAssets.A2Z1_Crate
-}
 
 A2Z1_lckCRT = '''
                                   [bold magenta]CRATE[/bold magenta]
-                                    ┬  
-                                    │  
-                                    │  
-                                    │  
-                                        
-                                    │  
-                                        
-                                    │  
-                                        
-                                    [bold rgb(128,128,128)]│[/bold rgb(128,128,128)]  
-'''
-
-A2Z1_ulckCRT = '''
-                                  [bold green]CRATE[/bold green]
                                     ┬  
                                     │  
                                     │  
